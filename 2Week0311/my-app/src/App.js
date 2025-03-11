7d992a8a2096f2dedfec50362f2bc469f33f34db
GUBBIB <moon.dragon250@gmail.com>
2025-03-11 11:12:15 +0900

    upload

---
+++ 2Week0311/my-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
+
+export default App;
