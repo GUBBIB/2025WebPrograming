b489f25af9b75fed572273ce5e934f2d29daeae0
GUBBIB <moon.dragon250@gmail.com>
2025-03-15 01:15:32 +0900

    upload

---
+++ 2Week0311/my-introduce/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>나의 소개</h1>
+      <img src="/img/sodugom.png" alt="Sodugom" />
+      <p>안녕하세요. 저는 21학번입니다.</p>
+    </div>
+  );
+}
+
+export default App;
