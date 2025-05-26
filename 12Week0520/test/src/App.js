d80c7f1781fe434c7094373b23b899e5d1bf50fe
GUBBIB <moon.dragon250@gmail.com>
2025-05-26 17:24:16 +0900

    리액트 코드 push

---
+++ 12Week0520/test/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AttendanceBook from './component/AttendanceBook';
+import SignUp from './component/SignUp';
+
+function App() {
+  return (
+    <div>
+      <AttendanceBook />
+      <SignUp />
+    </div>
+  );
+}
+
+export default App;
